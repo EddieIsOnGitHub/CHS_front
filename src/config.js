2f32036820358b9c2904775ffadf088676841c5f
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2025-11-10 11:25:34 +0200

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 // config.js
-export const API_BASE_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, "");
+// export const API_BASE_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, "");
 
 
 //  http://localhost:1000
+
+export const API_BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
